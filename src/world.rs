ca35051c422430904cf38c0f9b7a037c8de65608
Fraser Balch <farseer.ulwithy@gmail.com>
2020-01-26 11:21:24 -0700

    added impl World

--- src/world.rs
+++ src/world.rs
@@ -10,14 +10,15 @@ pub struct World {
 
 impl Display for World {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        writeln!(f, "{}", self.get_world()).ok();
-        writeln!(f, "{}", self.get_allocators()).ok();
-        writeln!(f, "{}", self.get_state()).ok();
-
-        for arena in self.arenas.iter() {
-            writeln!(f, "{}", arena.get_struct()).ok();
-            writeln!(f, "{}", arena.get_data_row()).ok();
-        }
+//        writeln!(f, "{}", self.get_world()).ok();
+        writeln!(f, "{}", self.impl_world()).ok();
+//        writeln!(f, "{}", self.get_allocators()).ok();
+//        writeln!(f, "{}", self.get_state()).ok();
+//
+//        for arena in self.arenas.iter() {
+//            writeln!(f, "{}", arena.get_struct()).ok();
+//            writeln!(f, "{}", arena.get_data_row()).ok();
+//        }
 
         Ok(())
     }
@@ -41,6 +42,33 @@ impl World {
             .with_derives(Derives::with_debug_default_clone())
             .add_field(Field::from_type("Allocators"))
             .add_field(Field::from_type("State"))
+    }
+
+    pub fn impl_world(&self) -> Impl {
+        let world = self.get_world();
+
+        let params = "&mut self";
+
+        let mut return_type = String::from("(");
+        let mut code = String::from("(");
+        for (i, field) in world.fields.iter().enumerate() {
+            if i != 0 {
+                return_type.push_str(", ");
+                code.push_str(", ");
+            }
+
+            return_type.push_str(&format!("&mut {}", field.field_type));
+            code.push_str(&format!("&mut self.{}", field.name));
+        }
+        return_type.push(')');
+        code.push(')');
+
+        Impl::new(&world)
+            .add_function(Function::new("split")
+                .with_parameters(&params)
+                .with_return(&return_type)
+                .add_line(CodeLine::new(0, &code))
+            )
     }
 
     pub fn get_allocators(&self) -> Struct {
